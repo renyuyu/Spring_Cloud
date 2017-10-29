388a2ee3c688b743d96fd4741d94bed93c748b9e
renyuyu <renyyit@163.com>
2017-10-30 00:08:57 +0800

    All Spring Cloud Project Practice

---
+++ helloDemo/src/test/java/com/myexample/helloDemo/HelloDemoApplicationTests.java
@@ -0,0 +1,15 @@
+package com.myexample.helloDemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HelloDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
